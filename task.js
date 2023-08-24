bd51990ce3ea9b41f484dd80c2883e134b4154ab
sohel5G <contactsohelrana@gmail.com>
2023-08-24 23:36:26 +0600

    All done

--- task.js
+++ task.js
@@ -1,9 +1,7 @@
 //   Task 1
 
 // const funcName = (a, b, c) => a * b *c;
-// console.log( funcName(2, 3, 4) ); 
-
-
+// console.log( funcName(2, 3, 4) );
 
 // task 2
 
@@ -13,14 +11,10 @@
 // I love to eat biryani
 // `  )
 
-
-// Task 3 
+// Task 3
 
 // const funcName = (x, y = 0) => x + y;
-// console.log( funcName(6) ); 
-
-
-
+// console.log( funcName(6) );
 
 // Task 4
 
@@ -36,20 +30,13 @@
 // const myArray = ['Jamal', 'Ashik', 'Imrul', 'Sohel', 'Nila', 'Araf', 'Liza', 'Rana', 'Kamrul'];
 // console.log( evenLangthName(myArray) );
 
-
-
-
-// Task 5 
+// Task 5
 
 const findMaxNum = (arry1, array2) => {
-    const sumArray = [...arry1, ...array2];
-    return Math.max(...sumArray);
-}
+  const sumArray = [...arry1, ...array2];
+  return Math.max(...sumArray);
+};
 const arr1 = [12, 32, 2, 34, 125, 65, 35, 76, 88];
+
 const arr2 = [43, 45, 65, 31, 23, 78, 54, 99];
-console.log( findMaxNum(arr1, arr2) );
-
-
-
-
-
+console.log(findMaxNum(arr1, arr2));
